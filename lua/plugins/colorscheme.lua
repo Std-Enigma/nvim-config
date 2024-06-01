cda30bd32ce36d437b82df3b085a3685619e052f
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-01 03:59:39 +0330

    fix the transparency bug for neovide

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,8 +5,8 @@ return {
 		priority = 1000,
 		opts = {
 			style = "night",
-			transparent = true,
 			dim_inactive = true,
+			transparent = not vim.g.neovide, -- turn off transparency when using neovide because it doesn't work
 		},
 	},
 
@@ -18,7 +18,7 @@ return {
 		opts = {
 			term_colors = true,
 			show_end_of_buffer = true,
-			transparent_background = true,
+			transparent_background = not vim.g.neovide, -- turn off transparency when using neovide because it doesn't work
 			dim_inactive = { enabled = true },
 			integrations = {
 				hop = true,
@@ -33,7 +33,7 @@ return {
 				lsp_trouble = true,
 				illuminate = { lsp = true },
 				telescope = { style = "nvchad" },
-				colorful_winsep = { enabled = true },
+				-- colorful_winsep = { enabled = true },
 				indent_blankline = { scope_color = "lavender" },
 			},
 		},
