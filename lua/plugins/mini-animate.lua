25d3454d127b45f4f4243e9771cc71f07674cbea
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-01 19:29:37 +0330

    added mini-animate plugin

---
+++ lua/plugins/mini-animate.lua
@@ -0,0 +1,11 @@
+return {
+  "echasnovski/mini.animate",
+  event = "VeryLazy",
+  cond = not (vim.g.neovide or vim.g.vscode), -- don't load when using vscode or neovide
+  opts = {
+    open = { enable = false }, -- disable window openning animations because of a glitch in terminal
+    close = { enable = false }, -- disable window closing animations because of a glitch in terminal
+    resize = { enable = false }, -- disable split resizing animations because it breaks persistent resizing
+    scroll = { enable = false }, -- disable vertical scrolling animations because neoscroll already does it
+  },
+}
