efe96c6565c4d555743e8c57292c18300d5d0d67
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-01 17:15:26 +0330

    added a utility module for more configuration

---
+++ lua/util/init.lua
@@ -0,0 +1,11 @@
+---Utilities
+---
+---Various utility functions to use.
+---
+---This module can be loaded with `local util = require "util"`
+---
+---Code from AstroCore
+---https://github.com/AstroNvim/astrocore
+---@class util
+local M = {}
+return M
