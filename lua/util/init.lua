6b08034f6d83093977a4ee79ce5a731714c47414
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-01 22:35:14 +0330

    added more utilities to the util module

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -9,6 +9,23 @@
 ---@class util
 local M = {}
 
+--- Merge extended options with a default table of options
+---@param default? table The default table that you want to merge into
+---@param opts? table The new options that should be merged with the default table
+---@return table # The merged table
+function M.extend_tbl(default, opts)
+  opts = opts or {}
+  return default and vim.tbl_deep_extend("force", default, opts) or opts
+end
+
+--- Serve a notification with a title of Neovim
+---@param msg string The notification body
+---@param type integer|nil The type of the notification (:help vim.log.levels)
+---@param opts? table The nvim-notify options to use (:help notify-options)
+function M.notify(msg, type, opts)
+  vim.schedule(function() vim.notify(msg, type, M.extend_tbl({ title = "Neovim" }, opts)) end)
+end
+
 --- A helper function to wrap a module function to require a plugin before running
 ---@param plugin string The plugin to call `require("lazy").load` with
 ---@param module table The system module where the functions live (e.g. `vim.ui`)
