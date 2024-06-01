1725c5c39fd98c56f3620a5288b887e0cef8445c
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-02 03:26:21 +0330

    enabled openning and closing window animationts

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -3,8 +3,6 @@ return {
   event = "CursorMoved",
   cond = not (vim.g.neovide or vim.g.vscode), -- don't load when using vscode or neovide
   opts = {
-    open = { enable = false }, -- disable window openning animations because of a glitch in terminal
-    close = { enable = false }, -- disable window closing animations because of a glitch in terminal
     resize = { enable = false }, -- disable split resizing animations because it breaks persistent resizing
     scroll = { enable = false }, -- disable vertical scrolling animations because vim-smoothie already does it
   },
