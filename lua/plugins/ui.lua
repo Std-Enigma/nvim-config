76d3846faabb98f7733b43890a3557d4c10f42a1
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-01 07:42:38 +0330

    fix the nvim-notify opts

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -10,17 +10,6 @@ return {
   -- better and fancier vim.ui
   {
     "stevearc/dressing.nvim",
-    opts = {
-      stages = "static",
-      timeout = 3000,
-      max_height = function() return math.floor(vim.o.lines * 0.75) end,
-      max_width = function() return math.floor(vim.o.columns * 0.75) end,
-      on_open = function(win)
-        vim.wo[win].spell = false
-        vim.wo[win].conceallevel = 3
-        vim.api.nvim_win_set_config(win, { zindex = 100 }) 
-      end,
-    },
     init = function()
       ---@diagnostic disable-next-line: duplicate-set-field
       vim.ui.select = function(...)
@@ -39,7 +28,16 @@ return {
   {
     "rcarriga/nvim-notify",
     dependencies = "nvim-lua/plenary.nvim",
-    opts = {},
+    opts = {
+      stages = "fade_in_slide_out",
+      max_height = function() return math.floor(vim.o.lines * 0.75) end,
+      max_width = function() return math.floor(vim.o.columns * 0.75) end,
+      on_open = function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 }) 
+        vim.wo[win].spell = false
+        vim.wo[win].conceallevel = 3
+      end,
+    },
   },
 
   -- completely replaces the UI for messages, cmdline and the popupmenu.
