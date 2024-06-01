2c7869d7bb1f57539a21dc9a3f62788c3e19a683
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-01 04:33:09 +0330

    added colorful-winsep.nvim plugin

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,4 +1,12 @@
 return {
+  -- colorize active window seperators and animate switching between them
+  {
+    "nvim-zh/colorful-winsep.nvim",
+    event = "WinNew",
+    config = true,
+    opts = {},
+  },
+
   -- ui components
   { "MunifTanjim/nui.nvim", lazy = true },
 
