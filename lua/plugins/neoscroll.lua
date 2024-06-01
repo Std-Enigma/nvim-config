e5dc3d3ebdbfdfd61334e2ec8fb10530eb9ed1f3
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-02 00:09:08 +0330

    changed neoscroll.nvim plugin load method to window scroll

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -1,6 +1,6 @@
 return {
   "karb94/neoscroll.nvim",
-  event = "VeryLazy",
+  event = "WinScrolled",
   cond = not (vim.g.neovide or vim.g.vscode), -- don't load when using vscode or neovide
   opts = {},
 }
