9c118583e8a37a48ac94bf72092f42ea0c8e8eb6
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-02 07:18:22 +0330

    added more treesitter textobjects and keymaps

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -33,7 +33,8 @@ return {
     if require("util").is_available("mason.nvim") then
       require("lazy").load({ plugins = { "mason.nvim" } })
     end
-    opts.auto_install = vim.fn.executable("git") == 1 and vim.fn.executable("tree-sitter") == 1 -- only enable auto install if `tree-sitter` cli is installed
+    auto_install = vim.fn.executable "git" == 1 and vim.fn.executable "tree-sitter" == 1, -- only enable auto install if `tree-sitter` cli is installed
+    vim.fn.executable("tree-sitter") == 1 -- only enable auto install if `tree-sitter` cli is installed
     opts.ensure_installed = {
       -- Vim
       "vim",
@@ -99,10 +100,14 @@ return {
           ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
           ["af"] = { query = "@function.outer", desc = "around function " },
           ["if"] = { query = "@function.inner", desc = "inside function " },
-          ["ao"] = { query = "@loop.outer", desc = "around loop" },
-          ["io"] = { query = "@loop.inner", desc = "inside loop" },
+          ["al"] = { query = "@loop.outer", desc = "around loop" },
+          ["il"] = { query = "@loop.inner", desc = "inside loop" },
           ["aa"] = { query = "@parameter.outer", desc = "around argument" },
           ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
+          ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+          ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+          ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+          ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
         },
       },
       move = {
@@ -112,21 +117,29 @@ return {
           ["]k"] = { query = "@block.outer", desc = "Next block start" },
           ["]f"] = { query = "@function.outer", desc = "Next function start" },
           ["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
+          ["]c"] = { query = "@class.outer", desc = "Next class start" },
+          ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
         },
         goto_next_end = {
           ["]K"] = { query = "@block.outer", desc = "Next block end" },
           ["]F"] = { query = "@function.outer", desc = "Next function end" },
           ["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
+          ["]C"] = { query = "@class.outer", desc = "Next class end" },
+          ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
         },
         goto_previous_start = {
           ["[k"] = { query = "@block.outer", desc = "Previous block start" },
           ["[f"] = { query = "@function.outer", desc = "Previous function start" },
           ["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
+          ["[c"] = { query = "@class.outer", desc = "Previous class start" },
+          ["[l"] = { query = "@loop.outer", desc = "Previous loop start" },
         },
         goto_previous_end = {
           ["[K"] = { query = "@block.outer", desc = "Previous block end" },
           ["[F"] = { query = "@function.outer", desc = "Previous function end" },
           ["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
+          ["[C"] = { query = "@class.outer", desc = "Previous class end" },
+          ["[L"] = { query = "@loop.outer", desc = "Previous loop end" },
         },
       },
       swap = {
@@ -135,13 +148,33 @@ return {
           [">K"] = { query = "@block.outer", desc = "Swap next block" },
           [">F"] = { query = "@function.outer", desc = "Swap next function" },
           [">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
+          [">C"] = { query = "@class.outer", desc = "Swap next class" },
+          [">L"] = { query = "@loop.outer", desc = "Swap next loop" },
         },
         swap_previous = {
           ["<K"] = { query = "@block.outer", desc = "Swap previous block" },
           ["<F"] = { query = "@function.outer", desc = "Swap previous function" },
           ["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
+          ["<C"] = { query = "@class.outer", desc = "Swap previous class" },
+          ["<L"] = { query = "@loop.outer", desc = "Swap previous loop" },
         },
       },
     }
+  end,
+  config = function(plugin, opts)
+    local ts = require(plugin.main)
+    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+    -- vim way: ; goes to the direction you were moving.
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+    -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+
+    ts.setup(opts)
   end,
 }
