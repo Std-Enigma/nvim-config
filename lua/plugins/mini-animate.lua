c04aa07bb949b4ed7176cede4428879699e83f1e
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-03 20:54:18 +0330

    changed the default animations

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -2,7 +2,11 @@ return {
 	"echasnovski/mini.animate",
 	event = { "CursorMoved", "WinNew", "WinScrolled" },
 	cond = not (vim.g.neovide or vim.g.vscode), -- don't load when using vscode or neovide
-	opts = {
-		resize = { enable = false }, -- disable split resizing animations because it breaks persistent resizing
-	},
+  config = function(_, opts)
+    require("mini.animate").setup(opts)
+    MiniAnimate.config.resize.enable = false -- disable split resizing animations because it breaks persistent resizing
+    MiniAnimate.config.cursor.path = MiniAnimate.gen_path.angle()
+    MiniAnimate.config.open.winconfig = MiniAnimate.gen_winconfig.wipe()
+    MiniAnimate.config.close.winconfig = MiniAnimate.gen_winconfig.center()
+  end
 }
