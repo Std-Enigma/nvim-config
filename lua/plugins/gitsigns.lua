297a64e66e3503cde4c8f1ece59350d8e96b5905
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-03 21:54:12 +0330

    formatted all the files

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  enabled = vim.fn.executable "git" == 1,
-  event = "VeryLazy",
-  opts = {},
+	"lewis6991/gitsigns.nvim",
+	enabled = vim.fn.executable("git") == 1,
+	event = "VeryLazy",
+	opts = {},
 }
--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -1,6 +1,6 @@
 return {
-  "Wansmer/treesj",
-  cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
-  dependencies = "nvim-treesitter/nvim-treesitter",
-  opts = { use_default_keymaps = false },
+	"Wansmer/treesj",
+	cmd = { "TSJToggle", "TSJSplit", "TSJJoin" },
+	dependencies = "nvim-treesitter/nvim-treesitter",
+	opts = { use_default_keymaps = false },
 }
