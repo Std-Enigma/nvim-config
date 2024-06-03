050eed2201bfd7774e36ff01cb47f1031b6ed0b9
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-03 18:23:20 +0330

    Revert "removed vim-smoothie plugin and use mini-animate instead for scrolling"
    This reverts commit 19154e3a9e8cc42da0e050f9768d4152370fad65.

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -4,5 +4,6 @@ return {
 	cond = not (vim.g.neovide or vim.g.vscode), -- don't load when using vscode or neovide
 	opts = {
 		resize = { enable = false }, -- disable split resizing animations because it breaks persistent resizing
+		scroll = { enable = false }, -- disable vertical scrolling animations because vim-smoothie already does it
 	},
 }
---
+++ lua/plugins/vim-smoothie.lua
@@ -0,0 +1,5 @@
+return {
+	"psliwka/vim-smoothie",
+	event = "WinScrolled",
+	cond = not (vim.g.neovide or vim.g.vscode), -- don't load when using vscode or neovide
+}
