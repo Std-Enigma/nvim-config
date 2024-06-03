6d197bc4cad1abda99fbe2fd28421d09dcde1a87
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-03 23:36:49 +0330

    changed the telescope window layout

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,10 +12,9 @@ return {
       opts.defaults = {
         git_worktrees = { toplevel = vim.env.HOME, gitdir = vim.env.HOME .. "/.dotfiles" },
         path_display = { "truncate" },
-        sorting_strategy = "ascending",
         layout_config = {
-          -- horizontal = { prompt_position = "top", preview_width = 0.55 },
-          -- vertical = { mirror = false },
+          horizontal = { preview_width = 0.55 },
+          vertical = { mirror = false },
           width = 0.85,
           height = 0.80,
           preview_cutoff = 120,
