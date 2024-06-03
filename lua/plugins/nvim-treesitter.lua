1867a48090b554d8c9642231ee6d10603ef4672c
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-06-03 23:32:35 +0330

    changed some default treesitter mappings

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -111,10 +111,8 @@ return {
           ["il"] = { query = "@loop.inner", desc = "inside loop" },
           ["aa"] = { query = "@parameter.outer", desc = "around argument" },
           ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-          ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
-          ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
-          ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
-          ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+          ["a="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+          ["i="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
         },
       },
       move = {
